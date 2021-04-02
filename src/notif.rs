f0c24e800972e971072a83e2eafca7a8a8cdf234
desbma <desbma@users.noreply.github.com>
2021-04-02 23:07:59 +0200

    Handle urgency

--- src/notif.rs
+++ src/notif.rs
@@ -1,9 +1,17 @@
 use crate::gotify;
 
 pub fn show(msg: gotify::Message) -> anyhow::Result<()> {
+    let urgency = match msg.priority {
+        1..=3 => notify_rust::Urgency::Low,
+        4..=7 => notify_rust::Urgency::Normal,
+        8..=10 => notify_rust::Urgency::Critical,
+        _ => anyhow::bail!("Unexpected urgency value"),
+    };
+
     notify_rust::Notification::new()
         .summary(&msg.title)
         .body(&msg.message)
+        .urgency(urgency)
         .show()?;
 
     Ok(())
