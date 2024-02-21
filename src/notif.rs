45fd989aecef56e885063d949b3e2aa58c8d1ce3
desbma <desbma@users.noreply.github.com>
2024-02-21 13:39:56 +0100

    feat: fallback to desktop entry icon if no app icon is set

--- src/notif.rs
+++ src/notif.rs
@@ -2,6 +2,9 @@
 
 use crate::gotify;
 
+/// Name of the XDG Desktop entry, without the .desktop suffix
+const DESKTOP_ENTRY_NAME: &str = env!("CARGO_PKG_NAME");
+
 /// Show notification
 pub fn show(msg: &gotify::Message) -> anyhow::Result<()> {
     #[cfg(all(unix, not(target_os = "macos")))]
@@ -21,13 +24,17 @@ pub fn show(msg: &gotify::Message) -> anyhow::Result<()> {
     notif
         .urgency(urgency)
         .appname("Gotify Desktop")
-        .hint(notify_rust::Hint::DesktopEntry("gotify-desktop".to_owned()));
+        .hint(notify_rust::Hint::DesktopEntry(
+            DESKTOP_ENTRY_NAME.to_owned(),
+        ));
     if let Some(img_filepath) = &msg.app_img_filepath.as_ref() {
         notif.icon(
             img_filepath
                 .to_str()
                 .ok_or_else(|| anyhow::anyhow!("Unable to convert path to string"))?,
         );
+    } else {
+        notif.icon(DESKTOP_ENTRY_NAME);
     }
 
     notif.show()?;
