e235c81399dfc9a91a72e8a6c4c2913f3fef807f
desbma <desbma@users.noreply.github.com>
2024-02-21 13:34:30 +0100

    feat: set app in notifications

--- src/notif.rs
+++ src/notif.rs
@@ -18,7 +18,10 @@ pub fn show(msg: &gotify::Message) -> anyhow::Result<()> {
     let mut notif = notify_rust::Notification::new();
     notif.summary(&msg.title).body(&msg.message);
     #[cfg(all(unix, not(target_os = "macos")))]
-    notif.urgency(urgency);
+    notif
+        .urgency(urgency)
+        .appname("Gotify Desktop")
+        .hint(notify_rust::Hint::DesktopEntry("gotify-desktop".to_owned()));
     if let Some(img_filepath) = &msg.app_img_filepath.as_ref() {
         notif.icon(
             img_filepath
