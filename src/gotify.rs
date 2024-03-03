desbma/gotify-desktop

commit ce5ca1f7944db300c86ca2cbec1fc7be86e03e19

refactor: simplify client state

src/gotify.rs
@@ -25,13 +25,10 @@ pub enum NeedsReconnect {
 
 /// Gotify client state
 pub struct Client {
-    /// Local config
-    config: config::GotifyConfig,
-
     /// Websocket client, if connected
-    ws: Option<WebSocket>,
+    ws: WebSocket,
     /// Socket poller
-    poller: Option<mio::Poll>,
+    poller: mio::Poll,
 
     /// HTTP client (non websocket)
     #[allow(clippy::struct_field_names)]
@@ -103,15 +100,15 @@ lazy_static::lazy_static! {
 }
 
 impl Client {
-    /// Constructor
-    pub fn new(config: &config::GotifyConfig) -> anyhow::Result<Client> {
+    /// Get a connected Gotify client
+    pub fn connect(cfg: &config::GotifyConfig) -> anyhow::Result<Self> {
         // Init app img cache
         let app_imgs: HashMap<i64, Option<PathBuf>> = HashMap::new();
         let binary_name = env!("CARGO_PKG_NAME");
         let xdg_dirs = xdg::BaseDirectories::with_prefix(binary_name)?;
 
         // Http client (non WS)
-        let mut gotify_header = reqwest::header::HeaderValue::from_str(&config.token)?;
+        let mut gotify_header = reqwest::header::HeaderValue::from_str(&cfg.token)?;
         gotify_header.set_sensitive(true);
         let mut http_headers = reqwest::header::HeaderMap::new();
         http_headers.insert("X-Gotify-Key", gotify_header);
@@ -119,29 +116,16 @@ impl Client {
             .user_agent(&*USER_AGENT)
             .default_headers(http_headers)
             .build()?;
-        let mut url = config.url.clone();
-        let scheme = match url.scheme() {
+        let mut http_url = cfg.url.clone();
+        let scheme = match http_url.scheme() {
             "wss" => "https",
             "ws" => "http",
             s => anyhow::bail!("Unexpected scheme {:?}", s),
         };
         #[allow(clippy::unwrap_used)] // We know the scheme is valid here
-        url.set_scheme(scheme).unwrap();
+        http_url.set_scheme(scheme).unwrap();
 
-        Ok(Client {
-            config: config.to_owned(),
-            ws: None,
-            poller: None,
-            http_client,
-            http_url: url,
-            app_imgs,
-            xdg_dirs,
-            last_msg_id: None,
-        })
-    }
-
-    /// Connect gotify client, with retries
-    pub fn connect(&mut self) -> anyhow::Result<()> {
+        // Connect gotify client, with retries
         let log_failed_attempt = |err, duration| {
             log::warn!("Connection failed: {}, retrying in {:?}", err, duration);
         };
@@ -154,28 +138,38 @@ impl Client {
             max_elapsed_time: None,
             ..backoff::ExponentialBackoff::default()
         };
-        backoff::retry_notify(
+        let (ws, poller) = backoff::retry_notify(
             retrier,
-            || self.try_connect().map_err(backoff::Error::transient),
+            || Self::try_connect(&cfg.url, &cfg.token).map_err(backoff::Error::transient),
             log_failed_attempt,
         )
         .map_err(|e| match e {
             backoff::Error::Permanent(e) => e,
             backoff::Error::Transient { err, .. } => err,
+        })?;
+
+        Ok(Self {
+            ws,
+            poller,
+            http_client,
+            http_url,
+            app_imgs,
+            xdg_dirs,
+            last_msg_id: None,
         })
     }
 
     /// Connect gotify client
-    fn try_connect(&mut self) -> anyhow::Result<()> {
+    fn try_connect(url: &url::Url, token: &str) -> anyhow::Result<(WebSocket, mio::Poll)> {
         // WS connect & handshake
-        let mut url = self.config.url.clone();
+        let mut url = url.to_owned();
         url.path_segments_mut()
-            .map_err(|()| anyhow::anyhow!("Invalid URL {}", self.config.url))?
+            .map_err(|()| anyhow::anyhow!("Invalid URL"))?
             .push("stream");
         let mut request = url.into_client_request()?;
         let headers = request.headers_mut();
         headers.insert("User-Agent", HeaderValue::from_str(&USER_AGENT)?);
-        headers.insert("X-Gotify-Key", HeaderValue::from_str(&self.config.token)?);
+        headers.insert("X-Gotify-Key", HeaderValue::from_str(token)?);
         let (mut ws, response) = tungstenite::connect(request)?;
 
         // Check response
@@ -203,9 +197,7 @@ impl Client {
             mio::Interest::READABLE,
         )?;
 
-        self.ws = Some(ws);
-        self.poller = Some(poller);
-        Ok(())
+        Ok((ws, poller))
     }
 
     /// Catch up missed messages since the last received one
@@ -247,21 +239,12 @@ impl Client {
 
     /// Get pending gotify messages
     pub fn get_message(&mut self) -> anyhow::Result<Message> {
-        let ws = self
-            .ws
-            .as_mut()
-            .ok_or_else(|| anyhow::anyhow!("Not connected"))?;
-        let poller = self
-            .poller
-            .as_mut()
-            .ok_or_else(|| anyhow::anyhow!("Not connected"))?;
-
         loop {
             // Poll to detect stale socket, so we can trigger reconnect,
             // this can occur when returning from sleep/hibernation
             // Without this, read_message blocks forever even if server already closed its end
             let mut poller_events = mio::Events::with_capacity(1);
-            let poll_res = poller.poll(&mut poller_events, None);
+            let poll_res = self.poller.poll(&mut poller_events, None);
             match poll_res {
                 Err(e) if e.kind() == ErrorKind::Interrupted => {
                     return Err(NeedsReconnect::Io(e).into());
@@ -275,7 +258,7 @@ impl Client {
             log::trace!("Event: {:?}", poller_events);
 
             // Read message
-            let read_res = ws.read();
+            let read_res = self.ws.read();
             let ws_msg = match read_res {
                 Ok(m) => m,
                 Err(tungstenite::Error::Protocol(e))
@@ -291,7 +274,7 @@ impl Client {
             let msg_str = match ws_msg {
                 tungstenite::protocol::Message::Text(msg_str) => msg_str,
                 tungstenite::protocol::Message::Ping(_) => {
-                    ws.flush()?;
+                    self.ws.flush()?;
                     continue;
                 }
                 _ => anyhow::bail!("Unexpected message type: {:?}", ws_msg),

src/main.rs
@@ -81,13 +81,11 @@ fn main() -> anyhow::Result<()> {
         ),
     };
 
-    // Init client
-    let mut client = gotify::Client::new(&cfg.gotify).context("Failed to setup client")?;
-
     // Connect loop
     loop {
         // Connect
-        client.connect().context("Failed to connect")?;
+        let mut client =
+            gotify::Client::connect(&cfg.gotify).context("Failed to setup or connect client")?;
         log::info!("Connected to {}", cfg.gotify.url);
 
         // Handle missed messages

.pre-commit-config.yaml
@@ -45,8 +45,7 @@ repos:
         stages: [commit]
 
   - repo: https://github.com/compilerla/conventional-pre-commit
-    rev: v1.0.0
+    rev: v3.1.0
     hooks:
       - id: conventional-pre-commit
         stages: [commit-msg]
-        args: [chore, config, doc, test]

commit b6150d91bc09eeb42661c426de82e8702e0e2d8d

fix: missed messages feature broken in ce5ca1f

src/gotify.rs
@@ -1,10 +1,12 @@
 //! Gotify network & parsing code
 
 use std::{
+    cell::RefCell,
     collections::HashMap,
     io::ErrorKind,
     os::unix::io::AsRawFd,
     path::{Path, PathBuf},
+    rc::Rc,
     time::Duration,
 };
 
@@ -42,7 +44,7 @@ pub struct Client {
     xdg_dirs: xdg::BaseDirectories,
 
     /// Last received Gotify message id
-    last_msg_id: Option<i64>,
+    last_msg_id: Rc<RefCell<Option<i64>>>,
 }
 
 /// Gotify message
@@ -101,7 +103,10 @@ lazy_static::lazy_static! {
 
 impl Client {
     /// Get a connected Gotify client
-    pub fn connect(cfg: &config::GotifyConfig) -> anyhow::Result<Self> {
+    pub fn connect(
+        cfg: &config::GotifyConfig,
+        last_msg_id: Rc<RefCell<Option<i64>>>,
+    ) -> anyhow::Result<Self> {
         // Init app img cache
         let app_imgs: HashMap<i64, Option<PathBuf>> = HashMap::new();
         let binary_name = env!("CARGO_PKG_NAME");
@@ -155,7 +160,7 @@ impl Client {
             http_url,
             app_imgs,
             xdg_dirs,
-            last_msg_id: None,
+            last_msg_id,
         })
     }
 
@@ -202,36 +207,37 @@ impl Client {
 
     /// Catch up missed messages since the last received one
     pub fn get_missed_messages(&mut self) -> anyhow::Result<Vec<Message>> {
-        let mut missed_messages: Vec<Message> = if let Some(last_msg_id) = self.last_msg_id {
-            // Get all recent messages
-            let mut url = self.http_url.clone();
-            url.path_segments_mut()
-                .map_err(|()| anyhow::anyhow!("Invalid URL {}", self.http_url))?
-                .push("message");
-            url.query_pairs_mut().append_pair("limit", "200");
-            log::debug!("{}", url);
-            let response = self.http_client.get(url).send()?.error_for_status()?;
-            let json_data = response.text()?;
-            log::trace!("{}", json_data);
+        let mut missed_messages: Vec<Message> =
+            if let Some(last_msg_id) = *self.last_msg_id.borrow() {
+                // Get all recent messages
+                let mut url = self.http_url.clone();
+                url.path_segments_mut()
+                    .map_err(|()| anyhow::anyhow!("Invalid URL {}", self.http_url))?
+                    .push("message");
+                url.query_pairs_mut().append_pair("limit", "200");
+                log::debug!("{}", url);
+                let response = self.http_client.get(url).send()?.error_for_status()?;
+                let json_data = response.text()?;
+                log::trace!("{}", json_data);
 
-            // Parse response & keep the ones we have not yet seen
-            let all_messages: AllMessages = serde_json::from_str(&json_data)?;
-            all_messages
-                .messages
-                .into_iter()
-                .filter(|m| m.id > last_msg_id)
-                .rev()
-                .collect()
-        } else {
-            vec![]
-        };
+                // Parse response & keep the ones we have not yet seen
+                let all_messages: AllMessages = serde_json::from_str(&json_data)?;
+                all_messages
+                    .messages
+                    .into_iter()
+                    .filter(|m| m.id > last_msg_id)
+                    .rev()
+                    .collect()
+            } else {
+                vec![]
+            };
 
         for missed_message in &mut missed_messages {
             self.set_message_app_img(missed_message)?;
         }
 
         if let Some(last_msg) = missed_messages.iter().last() {
-            self.last_msg_id = Some(last_msg.id);
+            self.last_msg_id.replace(Some(last_msg.id));
         }
 
         Ok(missed_messages)
@@ -287,7 +293,7 @@ impl Client {
             // Get app image
             self.set_message_app_img(&mut msg)?;
 
-            self.last_msg_id = Some(msg.id);
+            self.last_msg_id.replace(Some(msg.id));
             return Ok(msg);
         }
     }

src/main.rs
@@ -1,6 +1,6 @@
 //! Gotify desktop daemon
 
-use std::process::Command;
+use std::{cell::RefCell, process::Command, rc::Rc};
 
 use anyhow::Context;
 
@@ -81,11 +81,14 @@ fn main() -> anyhow::Result<()> {
         ),
     };
 
+    // Keep last handled message id
+    let last_msg_id = Rc::new(RefCell::new(None));
+
     // Connect loop
     loop {
         // Connect
-        let mut client =
-            gotify::Client::connect(&cfg.gotify).context("Failed to setup or connect client")?;
+        let mut client = gotify::Client::connect(&cfg.gotify, Rc::clone(&last_msg_id))
+            .context("Failed to setup or connect client")?;
         log::info!("Connected to {}", cfg.gotify.url);
 
         // Handle missed messages